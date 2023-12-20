9438cdf0b272212f6d062c94735502f7cc42442f
slerpxcq <hyptrszmx3244@gmail.com>
2023-12-20 15:29:26 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cflags="-Wall -pedantic -std=gnu11 -ggdb"
-gcc main.c -o main.o ${cflags}
-gcc pmx_model.c -o pmx_model.o ${cflags}
+gcc main.c -o main.o -c ${cflags}
+gcc pmx_model.c -o pmx_model.o -c ${cflags}
 gcc main.o pmx_model.o -o main
